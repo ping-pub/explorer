995fc3d7bb74ff9ab25e3ffe0b3b6eb6ae4414eb
liangping <18786721@qq.com>
2021-09-20 20:35:40 +0800

    Disable gravity for now

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -26,11 +26,11 @@ const modules = [
     title: 'uptime',
     route: 'uptime',
   },
-  {
-    scope: 'cosmos',
-    title: 'gravity',
-    route: 'gravity',
-  },
+  // {
+  //   scope: 'cosmos',
+  //   title: 'gravity',
+  //   route: 'gravity',
+  // },
 ]
 
 function processMenu() {
