0ccfd3b79f0e9449a45cc6e63384d1939ece661f
liangping <18786721@qq.com>
2021-11-17 15:28:09 +0800

    refactor

--- src/store/chains/index.js
+++ src/store/chains/index.js
@@ -1,6 +1,9 @@
 let chains = {}
 
-const configs = require.context('.', false, /\.json$/)
+let configs = require.context('../../chains/mainnet', false, /\.json$/)
+if (window.location.hostname.startsWith('testnet') || window.location.search.indexOf('testnet')) {
+  configs = require.context('../../chains/testnet', false, /\.json$/)
+}
 
 const update = {}
 configs.keys().forEach(k => {
@@ -8,37 +11,9 @@ configs.keys().forEach(k => {
   update[c.chain_name] = c
 })
 
-// const localChains = localStorage.getItem('chains')
-// if (localChains) {
-//   chains = JSON.parse(localChains)
-// } else {
 chains = update
 localStorage.setItem('chains', JSON.stringify(update))
 const selected = chains.cosmos
-
-// }
-
-// Object.keys(update).forEach(key => {
-//   const chain = update[key]
-//   if (!chain.sdk_version) {
-//     fetch(`${chain.api}/node_info`)
-//       .then(res => res.json())
-//       .then(json => {
-//         const { build_deps } = json.application_version
-//         // eslint-disable-next-line camelcase
-//         if (build_deps) {
-//           const sdk = build_deps.find(e => e.startsWith('github.com/cosmos/cosmos-sdk'))
-//           const re = /(\d+(\.\d+)*)/i
-//           const version = sdk.match(re)
-//           // eslint-disable-next-line prefer-destructuring
-//           chain.sdk_version = version[0]
-//         } else {
-//           chain.sdk_version = json.node_info.version
-//         }
-//         localStorage.setItem('chains', JSON.stringify(update))
-//       })
-//   }
-// })
 
 export default {
   namespaced: true,
