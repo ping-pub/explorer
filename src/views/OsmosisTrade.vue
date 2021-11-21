36b424cff234f36c779b41fdd9d5943fddd874b8
liangping <18786721@qq.com>
2021-11-21 09:55:55 +0800

    add fetch ohlc

--- src/views/OsmosisTrade.vue
+++ src/views/OsmosisTrade.vue
@@ -3,27 +3,16 @@
     -------
     <b-row class="match-height">
       <b-col
-        v-for="(data,index) in pools.pools"
-        :key="index"
         md="4"
         sm="6"
       >
-        <router-link :to="data.id">
+        <router-link to="">
           <b-card
-            v-if="data"
             class="earnings-card text-left"
           >
             <b-row>
               <b-col cols="8">
-                <b-card-title class="mb-1 text-uppercase">
-                  #{{ data.id }} {{ formatDenom(data.poolAssets[0].token.denom) }} - {{ formatDenom(data.poolAssets[1].token.denom) }}<small class="font-small-2"> xx</small>
-                </b-card-title>
-                <div class="font-small-2">
-                  Height
-                </div>
-                <h5 class="mb-1">
-                  {{ data.address || '0' }}
-                </h5>
+                x
               </b-col>
             </b-row>
           </b-card>
@@ -35,7 +24,7 @@
 
 <script>
 import {
-  BCard, BCardTitle, VBTooltip, BRow, BCol,
+  BCard, VBTooltip, BRow, BCol,
 } from 'bootstrap-vue'
 import { formatTokenDenom } from '@/libs/data'
 
@@ -44,22 +33,38 @@ export default {
     BRow,
     BCol,
     BCard,
-    BCardTitle,
   },
   directives: {
     'b-tooltip': VBTooltip,
   },
   data() {
     return {
+      trade_pairs: [
+        {
+          from: {
+            denom: 'atom',
+            origin_denom: 'atom',
+            coingecko_id: 'cosmos',
+          },
+          to: {
+            denom: 'osmo',
+            origin_denom: 'osmos',
+            coingecko_id: 'osmosis',
+          },
+        },
+      ],
       pools: [],
       ibcDenom: {},
     }
   },
   created() {
     // 所有方法添加到 $http.osmosis
-    this.$http.osmosis.getPools().then(res => {
-      this.pools = res
+    this.$http.osmosis.getOHCL4Pairs('cosmos', 'osmosis').then(data => {
+      console.log(data)
     })
+    // this.$http.osmosis.getPools().then(res => {
+    //   this.pools = res
+    // })
   },
   beforeDestroy() {
     this.islive = false
