e9db729843905a2ff4781498f8648bdfd65e1f4a
liangping <18786721@qq.com>
2021-12-11 11:32:55 +0800

    remove debugs

--- src/libs/data/signing.js
+++ src/libs/data/signing.js
@@ -21,7 +21,7 @@ export default class PingWalletClient extends SigningStargateClient {
     // console.log('registory:', instance.registry, AminoTypes)
     const { aminoTypes = new AminoTypes({ prefix: options.prefix }) } = options
     instance.aminoTypes = aminoTypes
-    console.log('aminoType:', instance.aminoTypes)
+    // console.log('aminoType:', instance.aminoTypes)
     return instance
   }
 
@@ -33,16 +33,16 @@ export default class PingWalletClient extends SigningStargateClient {
     }
     const pubkey = proto_signing_1.encodePubkey(amino_1.encodeSecp256k1Pubkey(accountFromSigner.pubkey))
     const signMode = signing_1.SignMode.SIGN_MODE_LEGACY_AMINO_JSON
-    console.log('messages:', messages)
+    // console.log('messages:', messages)
     const msgs = messages.map(msg => this.aminoTypes.toAmino(msg))
-    console.log('msgs:', msgs)
+    // console.log('msgs:', msgs)
     const signDoc = amino_1.makeSignDoc(msgs, fee, chainId, memo, accountNumber, sequence)
     const { signature, signed } = await this.signer.signAmino(signerAddress, signDoc)
     const signedTxBody = {
       messages: signed.msgs.map(msg => this.aminoTypes.fromAmino(msg)),
       memo: signed.memo,
     }
-    console.log(signedTxBody)
+    // console.log(signedTxBody)
     const signedTxBodyEncodeObject = {
       typeUrl: '/cosmos.tx.v1beta1.TxBody',
       value: signedTxBody,
