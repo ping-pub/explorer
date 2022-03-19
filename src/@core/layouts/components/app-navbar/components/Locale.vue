2faacd1d6ba7827e44417052ed7f6ca7fa89ab59
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-03-19 02:48:59 +0200

    fix

--- src/@core/layouts/components/app-navbar/components/Locale.vue
+++ src/@core/layouts/components/app-navbar/components/Locale.vue
@@ -1,33 +1,33 @@
-#<template>
-#  <b-nav-item-dropdown
-#    id="dropdown-grouped"
-#    class="dropdown-language"
-#    right
-#  >
-#    <template #button-content>
-#      <b-img
-#        :src="currentLocale.img"
-#        height="14px"
-#        width="22px"
-#        :alt="currentLocale.locale"
-#      />
-#      <span class="ml-50 text-body d-none">{{ currentLocale.name }}</span>
-#    </template>
-#    <b-dropdown-item
-#      v-for="localeObj in locales"
-#      :key="localeObj.locale"
-#      @click="$i18n.locale = localeObj.locale"
-#    >
-#      <b-img
-#        :src="localeObj.img"
-#        height="14px"
-#        width="22px"
-#        :alt="localeObj.locale"
-#      />
-#      <span class="ml-50">{{ localeObj.name }}</span>
-#    </b-dropdown-item>
-#  </b-nav-item-dropdown>
-#</template>
+<template>
+  <b-nav-item-dropdown
+    id="dropdown-grouped"
+    class="dropdown-language"
+    right
+  >
+    <template #button-content>
+      <b-img
+        :src="currentLocale.img"
+        height="14px"
+        width="22px"
+        :alt="currentLocale.locale"
+      />
+      <span class="ml-50 text-body d-none">{{ currentLocale.name }}</span>
+    </template>
+    <b-dropdown-item
+      v-for="localeObj in locales"
+      :key="localeObj.locale"
+      @click="$i18n.locale = localeObj.locale"
+    >
+      <b-img
+        :src="localeObj.img"
+        height="14px"
+        width="22px"
+        :alt="localeObj.locale"
+      />
+      <span class="ml-50">{{ localeObj.name }}</span>
+    </b-dropdown-item>
+  </b-nav-item-dropdown>
+</template>
 
 <script>
 import { BNavItemDropdown, BDropdownItem, BImg } from 'bootstrap-vue'
