cb11ecaccf2549a8092f45e460ff9e5475ca6109
liangping <18786721@qq.com>
2022-08-31 20:03:42 +0800

    reduce gov size

--- src/libs/fetch.js
+++ src/libs/fetch.js
@@ -352,10 +352,9 @@ export default class ChainFetch {
   async getGovernanceList(next = '', chain = null) {
     const key = next || ''
     const url = this.config.chain_name === 'shentu'
-      ? `/shentu/gov/v1alpha1/proposals?pagination.limit=50&pagination.reverse=true&pagination.key=${key}`
-      : `/cosmos/gov/v1beta1/proposals?pagination.limit=50&pagination.reverse=true&pagination.key=${key}`
+      ? `/shentu/gov/v1alpha1/proposals?pagination.limit=20&pagination.reverse=true&pagination.key=${key}`
+      : `/cosmos/gov/v1beta1/proposals?pagination.limit=20&pagination.reverse=true&pagination.key=${key}`
     return this.get(url, chain).then(data => {
-      // const pool = new StakingPool().init(commonProcess(data[1]))
       let proposals = commonProcess(data)
       if (Array.isArray(proposals.proposals)) {
         proposals = proposals.proposals
