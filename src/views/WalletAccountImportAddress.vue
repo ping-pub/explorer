1656da33ed7d65883ce46095220aeffcbcb322c5
muhammadp <muhammad.perreira@onfinality.io>
2022-09-02 10:10:46 -0400

    typo string

--- src/views/WalletAccountImportAddress.vue
+++ src/views/WalletAccountImportAddress.vue
@@ -466,7 +466,7 @@ export default {
     initParamsForKeplr(chainid, chain) {
       return JSON.stringify({
         chainId: chainid,
-        chainName: Point,
+        chainName: 'Point',
         rpc: Array.isArray(chain.rpc) ? chain.rpc[0] : chain.rpc,
         rest: Array.isArray(chain.api) ? chain.api[0] : chain.api,
         bip44: {
