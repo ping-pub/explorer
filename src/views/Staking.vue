62de3a5b4e2daf4af39fe3ed44cd87817108d629
muhammadp <muhammad.perreira@onfinality.io>
2022-09-06 18:11:40 -0400

    test

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -56,11 +56,22 @@
         </template>
         <!-- Verified Point Validators -->
         <template #cell(point_validators)="data">
+          <small
+            v-if="data.item.changes>0"
+            class="text-success"
+          >+{{ data.item.changes }}</small>
+          <small v-else-if="data.item.changes===0">-</small>
+          <small
+            v-else
+            class="text-danger"
+          >{{ data.item.changes }}</small>
+          <!--
           <ul>
             <li v-for="node in data.item.point_validators" v-bind:key="node.name">
               {{ node.name }} - {{ node.id }}
             </li>
           </ul>
+          -->
         </template>
         <!-- Token -->
         <template #cell(tokens)="data">
