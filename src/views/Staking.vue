994d6ae4c14d9660884425d76b2bf9f9d8c5fa39
muhammadp <muhammad.perreira@onfinality.io>
2022-09-06 19:29:12 -0400

    fix

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -63,7 +63,7 @@
           <small
             v-else
             class="text-danger"
-          >{{ data.item.point_validators.id }}</small>
+          >{{ data.item.description.moniker }}</small>
         </template>
         <!-- Token -->
         <template #cell(tokens)="data">
