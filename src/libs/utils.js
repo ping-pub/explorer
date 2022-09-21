3b571df294f5d4cffe4bf7670ee04afaf5b38d4b
muhammadp <muhammad@pointlabs.org>
2022-09-21 14:14:28 -0400

    use apoint denom instead of uatom

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -429,7 +429,7 @@ export function numberWithCommas(x) {
   return parts.join('.')
 }
 
-export function formatTokenAmount(tokenAmount, decimals = 2, tokenDenom = 'uatom', format = true) {
+export function formatTokenAmount(tokenAmount, decimals = 2, tokenDenom = 'apoint', format = true) {
   const denom = tokenDenom.denom_trace ? tokenDenom.denom_trace.base_denom : tokenDenom
   let amount = 0
   let exp = String(denom).startsWith('gravity') ? 18 : 6
