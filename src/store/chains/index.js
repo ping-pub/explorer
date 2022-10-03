7e30c32d03e10ed5279c3c1dae4b4d28f5a84026
Polkachu <hello@polkachu.com>
2022-10-03 09:17:19 -0700

    add empower testnet

--- src/store/chains/index.js
+++ src/store/chains/index.js
@@ -12,7 +12,7 @@ import { toHex } from '@cosmjs/encoding'
 let chains = {}
 
 let configs = require.context('../../chains/mainnet', false, /\.json$/)
-if (isTestnet()) {
+if (true) {
   configs = require.context('../../chains/testnet', false, /\.json$/)
 }
 
