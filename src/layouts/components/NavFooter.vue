9c282d4c640b434b17f062c5d42ff41495493d20
Alisa | Side.one <alisa@side.one>
2023-05-31 15:51:27 +0800

    feat: remove unused style

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -4,7 +4,6 @@
   <footer
     class="flex sm:!flex-row flex-col items-center h-10 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full"
   >
-  <!-- items-center grid-flow-col -->
     <div class="flex flex-1">
       &copy;&nbsp;
       {{ new Date().getFullYear() }}&nbsp;
@@ -19,7 +18,6 @@
         >Ping.pub</a
       >
     </div>
-    <!-- grid-flow-col gap-4 sm:!place-self-center hidden sm:!grid -->
     <div
       class="flex "
     >
@@ -38,12 +36,3 @@
     </div>
   </footer>
 </template>
-display: grid;
-width: 100%;
-grid-auto-flow: row;
-place-items: start;
-row-gap: 2.5rem;
--moz-column-gap: 1rem;
-column-gap: 1rem;
-font-size: 0.875rem;
-line-height: 1.25rem;
