760596e02f19308fc4a9a370b9aba3ca6bd4ea6b
liangping <18786721@qq.com>
2023-06-08 12:38:33 +0800

    link on logo

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -60,10 +60,12 @@ function selected(route: any, nav: NavLink) {
       :class="{ block: sidebarShow, 'hidden xl:!block': !sidebarShow }"
     >
       <div class="flex items-center pl-4 py-4 mb-1">
-        <img class="w-10 h-10" src="../../assets/logo.svg" />
-        <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-          Ping.pub
-        </h1>
+        <RouterLink to="/" class="flex items-center">
+          <img class="w-10 h-10" src="../../assets/logo.svg" />
+          <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
+            Ping.pub
+          </h1>
+        </RouterLink>
         <div
           class="pr-4 cursor-pointer xl:!hidden"
           @click="sidebarShow = false"
