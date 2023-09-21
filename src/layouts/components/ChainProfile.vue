487472c05a9871fea4d4d6c71a86a1b2759da3d6
Salman Wahib <sxlmnwb.dev@gmail.com>
2023-09-21 22:10:17 +0000

    endpoint: improvement connectivity on dropdown
    Signed-off-by: Salman Wahib <hello@sxlmnwb.xyz>

--- src/layouts/components/ChainProfile.vue
+++ src/layouts/components/ChainProfile.vue
@@ -79,10 +79,14 @@ function changeEndpoint(item: Endpoint) {
       <div class="px-4 py-2 text-sm text-gray-400">Information</div>
       <div class="w-full">
         <div class="py-2 px-4">
-          Chain Id: {{ baseStore.latest.block?.header.chain_id }}
+          Chain Id: {{ baseStore.latest.block?.header.chain_id && baseStore.connected
+                        ? baseStore.latest.block.header.chain_id
+                        : 'N/A' }}
         </div>
         <div class="py-2 px-4">
-          Height: {{ baseStore.latest.block?.header.height }}
+          Height: {{ baseStore.latest.block?.header.height && baseStore.connected
+                      ? baseStore.latest.block.header.height
+                      : '0' }}
         </div>
       </div>
       <!-- bottom-->
