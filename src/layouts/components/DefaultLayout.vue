45bcb66988e7f6cae3cbeb14e4d9a1a48d83bc85
Francis Roch <francis@manafont.net>
2023-09-23 12:13:56 -0400

    Change `Ping.pub` branding in left menu to `burnt.com`

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -81,9 +81,9 @@ dayjs()
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img class="w-10 h-10" src="../../assets/logo.svg" />
+          <img class="w-10 h-10" src="https://raw.githubusercontent.com/cosmos/chain-registry/master/xion/images/burnt-round.png" />
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            Ping.pub
+            burnt.com
           </h1>
         </RouterLink>
         <div
