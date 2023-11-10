586f819d4198b332ba8ffe4132e0ec9c5d59b581
liangping <18786721@qq.com>
2023-11-10 15:33:48 +0800

    update status color

--- src/layouts/components/ChainProfile.vue
+++ src/layouts/components/ChainProfile.vue
@@ -17,7 +17,10 @@ function changeEndpoint(item: Endpoint) {
       <div class="p-1 relative mr-3 cursor-pointer">
         <img v-lazy="chainStore.logo" class="w-9 h-9 rounded-full" />
         <div
-          class="w-2 h-2 rounded-full bg-yes absolute right-0 bottom-0 shadow"
+          class="w-2 h-2 rounded-full absolute right-0 bottom-0 shadow" :class="{
+            'bg-success': baseStore.connected,
+            'bg-error': !baseStore.connected
+          }"
         ></div>
       </div>
       <div class="flex-1 w-0">
