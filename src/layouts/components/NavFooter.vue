3e3c888f7d6dc063f9bd578997418e6ca0d40a26
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-02-02 20:44:56 +0200

    feat: Modify footer text

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -6,15 +6,15 @@
     <div class="flex flex-1">
       &copy;&nbsp;
       {{ new Date().getFullYear() }}&nbsp;
-      Made With&nbsp;
+      Forked With &nbsp;
       <img src="../../assets/images/heart.svg" />&nbsp;
-      By&nbsp;
+      By &nbsp;
       <a
         class="link link-primary no-underline"
-        href="https://ping.pub"
+        href="https://whispernode.com"
         target="_blank"
         rel="noopener noreferrer"
-        >Ping.pub</a
+        >WhisperNode</a
       >
     </div>
     <div
