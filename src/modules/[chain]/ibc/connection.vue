8bf9c98d0cd030c67e5227db4d2b708ff91d5089
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-02-12 19:19:28 +0200

    fix: Add missing IBC styling

--- src/modules/[chain]/ibc/connection.vue
+++ src/modules/[chain]/ibc/connection.vue
@@ -40,7 +40,7 @@ function pageload(p: number) {
     <div class="bg-[#ffffff] dark:bg-[#222222] px-4 pt-3 pb-4 rounded shadow">
       <div class="flex flex-wrap gap-4  items-center">
         <h2 class="card-title py-4">{{ $t('ibc.title') }}</h2>
-        <div class="tabs tabs-boxed">
+        <div class="tabs tabs-boxed dark:bg-[#303030]">
           <a class="tab" :class="{ 'bg-[#ffea6e] text-black': tab === 'registry' }" @click="tab = 'registry'">{{ $t('ibc.registry') }}</a>
           <a class="tab" :class="{ 'bg-[#ffea6e] text-black': tab === 'favorite' }" @click="tab = 'favorite'">{{ $t('module.favorite') }}</a>
         </div>
@@ -53,7 +53,7 @@ function pageload(p: number) {
         <div v-show="tab === 'favorite'" class="flex flex-wrap gap-1 p-4 ">
           <div class="join border border-[#070707] dark:border-[#ffea6e]">
             <button class="join-item px-2">{{ $t('ibc.connection_id') }}:</button>
-            <input v-model="ibcStore.connectionId" type=number class="input  w-40 join-item border-s border-[#070707] dark:border-[#ffea6e] " min="0"
+            <input v-model="ibcStore.connectionId" type=number class="input  w-40 join-item dark:bg-[#222222] border-s border-[#070707] dark:border-[#ffea6e] " min="0"
               :max="pageResponse.total || 0" :placeholder="`0~${pageResponse.total}`" />
             <button class="join-item inline-flex items-center justify-center flex-wrap cursor-pointer select-none text-center transition duration-200 bg-[#ffea6e] text-black hover:bg-[#ffffff] dark:hover:bg-[#202020] dark:hover:text-white hover:shadow-[0_0_10px_3px_rgba(255,234,110,1)] font-bold px-2" @click="ibcStore.showConnection()">{{ $t('ibc.btn_apply') }}</button>
           </div>
