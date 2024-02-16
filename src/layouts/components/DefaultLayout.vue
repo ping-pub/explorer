8b6f7c0208bdf5c12b9292858b30b27367c9a5bd
WinNode <117538854+Winnode@users.noreply.github.com>
2024-02-16 07:08:41 +0700

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -67,9 +67,9 @@ function selected(route: any, nav: NavLink) {
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img class="w-10 h-10" src="../../assets/logo.svg" />
+          <img class="w-10 h-10" src="https://bafkreicgzyzegdmgjphlxzd4vxa2afdcy3kgjtzwjbpxmft4ju4dkymutq.ipfs.nftstorage.link/" />
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            Ping.pub
+            Winnode
           </h1>
         </RouterLink>
         <div
