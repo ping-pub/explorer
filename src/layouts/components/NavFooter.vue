a3e93291867b66e128f86873f6d5f671c4436df1
Francis Roch <francis@manafont.net>
2024-05-28 17:21:20 -0400

    Update footer

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -4,18 +4,18 @@
     class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full"
   >
     <div class="flex flex-1">
-      &copy;&nbsp;
-      {{ new Date().getFullYear() }}&nbsp;
-      Made With&nbsp;
-      <img src="../../assets/images/heart.svg" />&nbsp;
-      By&nbsp;
-      <a
-        class="link link-primary no-underline"
-        href="https://ping.pub"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Ping.pub</a
-      >
+<!--      &copy;&nbsp;-->
+<!--      {{ new Date().getFullYear() }}&nbsp;-->
+<!--      Made With&nbsp;-->
+<!--      <img src="../../assets/images/heart.svg" />&nbsp;-->
+<!--      By&nbsp;-->
+<!--      <a-->
+<!--        class="link link-primary no-underline"-->
+<!--        href="https://ping.pub"-->
+<!--        target="_blank"-->
+<!--        rel="noopener noreferrer"-->
+<!--        >Ping.pub</a-->
+<!--      >-->
     </div>
     <div
       class="hidden md:!block"
