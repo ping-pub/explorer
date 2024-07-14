a4e78a5301de801330759b3604eeb35cd0566a50
Muhammad Ehsan <syedehsans1@gmail.com>
2024-07-14 18:21:12 +0500

    Revert "reverted api target in vite config"
    This reverts commit 7eee547e3125b219bdf58bf457697c28cb7a0eea.

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,6 @@ export default defineConfig({
       imports: [
         'vue',
         'vue-router',
-        '@vueuse/core',
         '@vueuse/math',
         'vue-i18n',
         'pinia',
@@ -69,7 +68,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://explorer-pocket-poktroll-explorer-backend-1:3005',
+        target: 'http://localhost:3005',
         changeOrigin: true,
       },
     },
