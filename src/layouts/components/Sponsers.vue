d02eecc9630128f2bd63b378d342abb341bf4d74
liangping <18786721@qq.com>
2024-07-16 11:12:33 +0800

    update sponsers

---
+++ src/layouts/components/Sponsers.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <a href="https://cosmos.network" target="_blank"
+            class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]">
+            <img src="https://ping.pub/logos/cosmos.svg" class="w-6 h-6 rounded-full mr-3" />
+            <div class="text-sm capitalize flex-1 text-gray-600 dark:text-gray-200">
+                Cosmos Hub
+            </div>
+        </a>
+        <a href="https://osmosis.zone" target="_blank"
+            class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]">
+            <img src="https://ping.pub/logos/osmosis.jpg" class="w-6 h-6 rounded-full mr-3" />
+            <div class="text-sm capitalize flex-1 text-gray-600 dark:text-gray-200">
+                Osmosis
+            </div>
+        </a>
+        <a href="https://celestia.org" target="_blank"
+            class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]">
+            <img src="https://ping.pub/logos/celestia.png" class="w-6 h-6 rounded-full mr-3" />
+            <div class="text-sm capitalize flex-1 text-gray-600 dark:text-gray-200">
+                Celestia
+            </div>
+        </a>
+    </div>
+</template>
