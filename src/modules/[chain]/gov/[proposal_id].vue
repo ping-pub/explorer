8042f4f95e936a9242e7ffb0e3a7e8c6c5235b69
o-az <omaraziz.dev@proton.me>
2024-08-26 15:43:38 -0700

    fix: daisy colors

--- src/modules/[chain]/gov/[proposal_id].vue
+++ src/modules/[chain]/gov/[proposal_id].vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { computed } from 'vue';
-import MdEditor from 'md-editor-v3';
+import { MdEditor } from 'md-editor-v3';
 import ObjectElement from '@/components/dynamic/ObjectElement.vue';
 import {
   useBaseStore,
@@ -41,35 +41,35 @@ store.fetchProposal(props.proposal_id).then((res) => {
   }
   proposal.value = proposalDetail;
   // load origin params if the proposal is param change
-  if(proposalDetail.content?.changes) {
-    proposalDetail.content?.changes.forEach((item) => {  
-        chainStore.rpc.getParams(item.subspace, item.key).then((res) => {
-          if(proposal.value.content && res.param) {
-            if(proposal.value.content.current){
-              proposal.value.content.current.push(res.param);
-            } else {
-              proposal.value.content.current = [res.param];
-            };
-          }
-        })
+  if (proposalDetail.content?.changes) {
+    proposalDetail.content?.changes.forEach((item) => {
+      chainStore.rpc.getParams(item.subspace, item.key).then((res) => {
+        if (proposal.value.content && res.param) {
+          if (proposal.value.content.current) {
+            proposal.value.content.current.push(res.param);
+          } else {
+            proposal.value.content.current = [res.param];
+          };
+        }
+      })
     })
   }
 
   const msgType = proposalDetail.content['@type'] || '';
-  if(msgType.endsWith('MsgUpdateParams')) {
-    if(msgType.indexOf('staking') > -1) {
+  if (msgType.endsWith('MsgUpdateParams')) {
+    if (msgType.indexOf('staking') > -1) {
       chainStore.rpc.getStakingParams().then((res) => {
         addCurrentParams(res);
       });
-    } else if(msgType.indexOf('gov') > -1) {
+    } else if (msgType.indexOf('gov') > -1) {
       chainStore.rpc.getGovParamsVoting().then((res) => {
         addCurrentParams(res);
       });
-    } else if(msgType.indexOf('distribution') > -1) {
+    } else if (msgType.indexOf('distribution') > -1) {
       chainStore.rpc.getDistributionParams().then((res) => {
         addCurrentParams(res);
       });
-    } else if(msgType.indexOf('slashing') > -1) {
+    } else if (msgType.indexOf('slashing') > -1) {
       chainStore.rpc.getSlashingParams().then((res) => {
         addCurrentParams(res);
       });
@@ -78,7 +78,7 @@ store.fetchProposal(props.proposal_id).then((res) => {
 });
 
 function addCurrentParams(res: any) {
-  if(proposal.value.content && res.params) {
+  if (proposal.value.content && res.params) {
     proposal.value.content.params = [proposal.value.content?.params];
     proposal.value.content.current = [res.params];
   }
@@ -213,7 +213,7 @@ function pageload(p: number) {
   });
 }
 
-function metaItem(metadata: string|undefined): { title: string; summary: string } {
+function metaItem(metadata: string | undefined): { title: string; summary: string } {
   return metadata ? JSON.parse(metadata) : {}
 }
 </script>
@@ -223,30 +223,23 @@ function metaItem(metadata: string|undefined): { title: string; summary: string 
     <div class="bg-base-100 px-4 pt-3 pb-4 mb-4 shadow">
       <h2 class="card-title flex flex-col md:!justify-between md:!flex-row mb-2">
         <p class="truncate w-full">
-          {{ proposal_id }}. {{ proposal.title || proposal.content?.title || metaItem(proposal?.metadata)?.title  }}
+          {{ proposal_id }}. {{ proposal.title || proposal.content?.title || metaItem(proposal?.metadata)?.title }}
         </p>
-        <div
-          class="badge badge-ghost"
-          :class="
-            color === 'success'
-              ? 'text-yes'
-              : color === 'error'
+        <div class="badge badge-ghost" :class="color === 'success'
+            ? 'text-yes'
+            : color === 'error'
               ? 'text-no'
               : 'text-info'
-          "
-        >
+          ">
           {{ status }}
         </div>
       </h2>
       <div class="">
         <ObjectElement :value="proposal.content" />
       </div>
-      <div v-if="proposal.summary && !proposal.content?.description || metaItem(proposal?.metadata)?.summary ">
-        <MdEditor
-          :model-value="format.multiLine(proposal.summary || metaItem(proposal?.metadata)?.summary)"
-          previewOnly
-          class="md-editor-recover"
-        ></MdEditor>
+      <div v-if="proposal.summary && !proposal.content?.description || metaItem(proposal?.metadata)?.summary">
+        <MdEditor :model-value="format.multiLine(proposal.summary || metaItem(proposal?.metadata)?.summary)" previewOnly
+          class="md-editor-recover"></MdEditor>
       </div>
     </div>
     <!-- grid lg:!!grid-cols-3 auto-rows-max-->
@@ -258,37 +251,20 @@ function metaItem(metadata: string|undefined): { title: string; summary: string 
         <div class="mb-1" v-for="(item, index) of processList" :key="index">
           <label class="block text-sm mb-1">{{ item.name }}</label>
           <div class="h-5 w-full relative">
-            <div
-              class="absolute inset-x-0 inset-y-0 w-full opacity-10"
-              :class="`${item.class}`"
-            ></div>
-            <div
-              class="absolute inset-x-0 inset-y-0"
-              :class="`${item.class}`"
-              :style="`width: ${
-                item.value === '-' || item.value === 'NaN%' ? '0%' : item.value
-              }`"
-            ></div>
+            <div class="absolute inset-x-0 inset-y-0 w-full opacity-10" :class="`${item.class}`"></div>
+            <div class="absolute inset-x-0 inset-y-0" :class="`${item.class}`" :style="`width: ${item.value === '-' || item.value === 'NaN%' ? '0%' : item.value
+              }`"></div>
             <p
-              class="absolute inset-x-0 inset-y-0 text-center text-sm text-[#666] dark:text-[#eee] flex items-center justify-center"
-            >
+              class="absolute inset-x-0 inset-y-0 text-center text-sm text-[#666] dark:text-[#eee] flex items-center justify-center">
               {{ item.value }}
             </p>
           </div>
         </div>
         <div class="mt-6 grid grid-cols-2">
-          <label
-            for="vote"
-            class="btn btn-primary float-right btn-sm mx-1"
-            @click="dialog.open('vote', { proposal_id })"
-            >{{ $t('gov.btn_vote') }}</label
-          >
-          <label
-            for="deposit"
-            class="btn btn-primary float-right btn-sm mx-1"
-            @click="dialog.open('deposit', { proposal_id })"
-            >{{ $t('gov.btn_deposit') }}</label
-          >
+          <label for="vote" class="btn btn-primary float-right btn-sm mx-1"
+            @click="dialog.open('vote', { proposal_id })">{{ $t('gov.btn_vote') }}</label>
+          <label for="deposit" class="btn btn-primary float-right btn-sm mx-1"
+            @click="dialog.open('deposit', { proposal_id })">{{ $t('gov.btn_deposit') }}</label>
         </div>
       </div>
 
@@ -354,22 +330,18 @@ function metaItem(metadata: string|undefined): { title: string; summary: string 
             </div>
           </div>
 
-          <div
-            class="mt-4"
-            v-if="
-              proposal?.content?.['@type']?.endsWith('SoftwareUpgradeProposal')
-            "
-          >
+          <div class="mt-4" v-if="
+            proposal?.content?.['@type']?.endsWith('SoftwareUpgradeProposal')
+          ">
             <div class="flex items-center">
               <div class="w-2 h-2 bg-warning mr-3"></div>
               <div class="text-base flex-1 text-main">
                 {{ $t('gov.upgrade_plan') }}:
                 <span v-if="Number(proposal.content?.plan?.height || '0') > 0">
-                  (EST)</span
-                >
+                  (EST)</span>
                 <span v-else>{{
                   format.toDay(proposal.content?.plan?.time)
-                }}</span>
+                  }}</span>
               </div>
               <div class="text-sm">
                 {{ shortTime(proposal.voting_end_time) }}
@@ -390,30 +362,20 @@ function metaItem(metadata: string|undefined): { title: string; summary: string 
           <tbody>
             <tr v-for="(item, index) of votes" :key="index">
               <td class="py-2 text-sm">{{ showValidatorName(item.voter) }}</td>
-              <td
-                v-if="item.option"
-                class="py-2 text-sm"
-                :class="{
-                  'text-yes': item.option === 'VOTE_OPTION_YES',
-                  'text-gray-400': item.option === 'VOTE_OPTION_ABSTAIN',
-                }"
-              >
+              <td v-if="item.option" class="py-2 text-sm" :class="{
+                'text-yes': item.option === 'VOTE_OPTION_YES',
+                'text-gray-400': item.option === 'VOTE_OPTION_ABSTAIN',
+              }">
                 {{ String(item.option).replace('VOTE_OPTION_', '') }}
               </td>
-              <td
-                v-if="item.options"
-                class="py-2 text-sm"
-              >
-                {{ item.options.map(x => `${x.option.replace('VOTE_OPTION_', '')}:${format.percent(x.weight)}`).join(', ') }}
+              <td v-if="item.options" class="py-2 text-sm">
+                {{ item.options.map(x => `${x.option.replace('VOTE_OPTION_', '')}:${format.percent(x.weight)}`).join(',
+                ') }}
               </td>
             </tr>
           </tbody>
         </table>
-        <PaginationBar
-          :limit="pageRequest.limit"
-          :total="pageResponse.total"
-          :callback="pageload"
-        />
+        <PaginationBar :limit="pageRequest.limit" :total="pageResponse.total" :callback="pageload" />
       </div>
     </div>
   </div>
