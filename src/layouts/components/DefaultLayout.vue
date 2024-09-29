fa02609eb95da35b75cccf7357643d4556bed419
ywil23 <112842841+ywil23@users.noreply.github.com>
2024-09-29 11:15:24 +0700

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -83,7 +83,7 @@ dayjs()
         <RouterLink to="/" class="flex items-center">
           <img class="w-10 h-10" src="../../assets/logo.svg" />
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            Ping.pub
+            Vinjan.Inc
           </h1>
         </RouterLink>
         <div
