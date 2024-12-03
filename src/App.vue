f4d66e92e66e3e39d9afa8655575dfc9af337ff4
kahnix <kacperdev@gmail.com>
2024-12-03 13:30:30 +0100

    feat: add analytics

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { themeChange } from 'theme-change';
 import { onMounted } from 'vue';
 import TxDialog from './components/TxDialog.vue';
+import { Analytics } from '@vercel/analytics/vue';
 
 onMounted(() => {
   themeChange(false);
@@ -9,6 +10,7 @@ onMounted(() => {
 </script>
 
 <template>
+  <Analytics />
   <div>
     <RouterView />
     <TxDialog />
