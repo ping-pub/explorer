640134cf6f571675202a5db2c1d0f001eea8bea6
Muhammad Ehsan <syedehsans1@gmail.com>
2024-12-05 06:05:23 +0500

    updated stakenodes branding

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -4,7 +4,7 @@
     class="flex items-center h-12 mt-0 text-sm bg-gray-100 dark:bg-[#231f20;] py-2 z-10 w-full justify-end"
   >
     <div class="flex items-center justify-center">
-      Powered by: <a href="https://stakenodes.org" target="_blank"><img alt="https://stakenodes.org" src="https://stakenodes.org/logo.png" style="height:2rem"/></a> &nbsp;&nbsp;||&nbsp;&nbsp; 
+      Powered by: <a class="text-lg flex flex-row font-semibold items-center justify-center" href="https://stakenodes.org" target="_blank"><img alt="https://stakenodes.org" src="https://stakenodes.org/favicon.png" style="height:2rem"/>Stakenodes</a> &nbsp;&nbsp;||&nbsp;&nbsp; 
       Made With&nbsp;
       <img src="https://assets-global.website-files.com/651fe0a9a906d151784935f8/65c62e2727ed4e265bc9911a_universal-logo.png" width="20" />&nbsp;
       &copy;&nbsp;
