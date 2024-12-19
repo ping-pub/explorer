b4fdf5eb29f4d90b539e79ed3575e962fc3d0d3f
mastercodercat <mastercoderoy64@gmail.com>
2024-12-18 23:02:40 -0800

    feat(ui): clean up unnecessary stuff

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -3,20 +3,20 @@
   <footer
     class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full"
   >
+    <!--
     <div class="flex flex-1">
       &copy;&nbsp;
       {{ new Date().getFullYear() }}&nbsp;
-      Made With&nbsp;
-      <img src="../../assets/images/heart.svg" />&nbsp;
-      By&nbsp;
+      &nbsp;
       <a
         class="link link-primary no-underline"
-        href="https://ping.pub"
+        href="https://pastel.network"
         target="_blank"
         rel="noopener noreferrer"
-        >Ping.pub</a
+        >Pastel Network</a
       >
     </div>
+    -->
     <div
       class="hidden md:!block"
     >
