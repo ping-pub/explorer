c42bdef5302ab1c40e659b38d71c6474af863a8a
Tino Rusch <tino.rusch@gmail.com>
2025-02-20 08:52:41 +0100

    use toFixed(2) to limit decimals to two places.

--- src/modules/[chain]/block/[height].vue
+++ src/modules/[chain]/block/[height].vue
@@ -38,7 +38,7 @@ const remainingBlocks = computed(() => {
 })
 
 const estimateTime = computed(() => {
-  const seconds = remainingBlocks.value * store.blocktime
+  const seconds = Number((remainingBlocks.value * store.blocktime).toFixed(2))
   return seconds
 })
 
