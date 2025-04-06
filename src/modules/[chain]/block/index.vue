daa3e7539c05956c6ee9e24e264b897b5d9155fb
wizard <112275929+famouswizard@users.noreply.github.com>
2025-04-06 10:48:11 +0300

    fix: Incorrect Import Path for Vue 3 Composition API

--- src/modules/[chain]/block/index.vue
+++ src/modules/[chain]/block/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, ref } from '@vue/reactivity';
+import { computed, ref } from 'vue';
 import { useBaseStore, useFormatter } from '@/stores';
 import TxsInBlocksChart from '@/components/charts/TxsInBlocksChart.vue';
 
