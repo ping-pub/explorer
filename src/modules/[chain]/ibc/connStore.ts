5330553fd2decec92528231b5797ae6f4ff579d5
2xburnt <169301814+2xburnt@users.noreply.github.com>
2025-07-31 17:54:09 -0500

    ran prettier in ibc path

--- src/modules/[chain]/ibc/connStore.ts
+++ src/modules/[chain]/ibc/connStore.ts
@@ -1,7 +1,6 @@
-
 import { defineStore } from 'pinia';
 
-import { useBlockchain } from '@/stores'
+import { useBlockchain } from '@/stores';
 import { ChainRegistryClient } from '@chain-registry/client';
 import type { IBCData } from '@chain-registry/types/ibc_data.schema';
 import router from '@/router';
@@ -11,25 +10,25 @@ export const useIBCModule = defineStore('module-ibc', {
   state: () => {
     return {
       info: [] as IBCData[],
-      connectionId: "" as string,
+      connectionId: '' as string,
       registryConf: {} as IBCData,
     };
   },
   getters: {
     chain() {
-      return useBlockchain()
+      return useBlockchain();
     },
     chainName(): string {
       return this.chain.chainName;
     },
     sourceField(): string {
-      return this.registryConf?.chain_1?.chain_name === this.chainName ? 'chain_1' : 'chain_2'
+      return this.registryConf?.chain_1?.chain_name === this.chainName ? 'chain_1' : 'chain_2';
     },
     destField(): string {
-      return this.registryConf?.chain_1?.chain_name === this.chainName ? 'chain_2' : 'chain_1'
+      return this.registryConf?.chain_1?.chain_name === this.chainName ? 'chain_2' : 'chain_1';
     },
     registryChannels(): any {
-      return this.registryConf.channels
+      return this.registryConf.channels;
     },
   },
   actions: {
@@ -45,43 +44,41 @@ export const useIBCModule = defineStore('module-ibc', {
         });
 
         client.fetchUrls().then(() => {
-          const info = client.getChainIbcData(this.chainName)
+          const info = client.getChainIbcData(this.chainName);
           this.info = info.sort((a, b) => {
             // Sort by remote chain name (not equal to this.chainName)
-            const getRemote = (x: any) => x.chain_1.chain_name === this.chainName ? x.chain_2.chain_name : x.chain_1.chain_name;
+            const getRemote = (x: any) =>
+              x.chain_1.chain_name === this.chainName ? x.chain_2.chain_name : x.chain_1.chain_name;
             return getRemote(a).localeCompare(getRemote(b));
           });
-
-        })
+        });
       });
-
     },
     async fetchIBCUrls(): Promise<any[]> {
-      let ibcEndpoint = 'https://api.github.com/repos/cosmos/chain-registry/contents/_IBC'
-      if (this.chainName.includes("testnet")) {
-        console.log(this.chainName)
-        ibcEndpoint = 'https://api.github.com/repos/cosmos/chain-registry/contents/testnets/_IBC'
+      let ibcEndpoint = 'https://api.github.com/repos/cosmos/chain-registry/contents/_IBC';
+      if (this.chainName.includes('testnet')) {
+        console.log(this.chainName);
+        ibcEndpoint = 'https://api.github.com/repos/cosmos/chain-registry/contents/testnets/_IBC';
       }
-      const entries = await fetch(ibcEndpoint).then((res) => res.json())
+      const entries = await fetch(ibcEndpoint).then((res) => res.json());
       return entries.filter((x: any) => x.name.match(this.chainName));
     },
     fetchConnection(index: number) {
       const res = this.info[index];
-      const isFirstChain = res.chain_1.chain_name === this.chain.current?.prettyName || res.chain_1.chain_name === this.chain.chainName;
+      const isFirstChain =
+        res.chain_1.chain_name === this.chain.current?.prettyName || res.chain_1.chain_name === this.chain.chainName;
 
-      const connId = isFirstChain
-        ? res.chain_1.connection_id
-        : res.chain_2.connection_id;
+      const connId = isFirstChain ? res.chain_1.connection_id : res.chain_2.connection_id;
 
       this.registryConf = res;
       this.showConnection(connId);
     },
     showConnection(connId?: string | number) {
       if (!connId) {
-        this.registryConf = {} as any
+        this.registryConf = {} as any;
       }
-      const path = `/${this.chain.chainName}/ibc/connection/${connId || `connection-${this.connectionId || 0}`}`
-      router.push(path)
-    }
+      const path = `/${this.chain.chainName}/ibc/connection/${connId || `connection-${this.connectionId || 0}`}`;
+      router.push(path);
+    },
   },
 });
