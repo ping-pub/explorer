421e72afba918ff9c1d10ccb4bbb1e7650b84ef8
Muhammad Ehsan <syedehsans1@gmail.com>
2025-08-02 19:53:51 +0500

    updated indexer url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -117,7 +117,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://pocket_indexer_api:3005',
+        target: 'http://pocket_indexer_api:3006',
         // target: 'http://127.0.0.1:3005',
         changeOrigin: true,
       },
